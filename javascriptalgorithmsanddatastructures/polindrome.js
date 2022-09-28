8a734447f448100e365d000a97afb4297459b7c9
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-09-28 01:20:37 -0300

    248

--- javascriptalgorithmsanddatastructures/polindrome.js
+++ javascriptalgorithmsanddatastructures/polindrome.js
@@ -1,19 +1,31 @@
 // polindrome
 
-/*
+let inputPalindrome;
 
-function palindrome(str) {
-    const alphanumericOnly = str
-        // 1) Lowercase the input
-        .toLowerCase()
-        // 2) Strip out non-alphanumeric characters
-        .match(/[a-z0-9]/g);
-        
-    // 3) return string === reversedString
-    return alphanumericOnly.join('') ===
-        alphanumericOnly.reverse().join('');
+let poliResult = document.getElementById('poli-result');
+
+function palindromeCheck(){
+    inputPalindrome = document.getElementById('input-polindrome').value;
+    if(palindrome(inputPalindrome) == true){
+        poliResult.innerHTML = 'a Polidrome!'
+    }else if(palindrome(inputPalindrome) == false){
+        poliResult.innerHTML = 'NOT a Polidrome!'
+    }
 }
 
-palindrome("eye");
 
-*/
+function palindrome(str) {
+    if(str !== ''){
+        let alphanumericOnly = str;
+        // 1) Lowercase the input
+        alphanumericOnly = alphanumericOnly.toLowerCase()
+        // 2) Strip out non-alphanumeric characters
+        alphanumericOnly = alphanumericOnly.match(/[a-z0-9]/g);
+        // 3) return string === reversedString
+        return alphanumericOnly.join('') ===
+        alphanumericOnly.reverse().join('');
+    }else{
+        poliResult.innerHTML = 'mmm enter a input!'
+    }
+}
+
